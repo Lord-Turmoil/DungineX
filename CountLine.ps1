5959be85ee63802453fbb999349041467b0450d3
Lord Turmoil <swift-phantom@outlook.com>
2024-10-21 19:40:52 +0800

    update: use cloc to count lines

--- CountLine.ps1
+++ CountLine.ps1
@@ -1,18 +1,1 @@
-# save current directory
-$pwd = Get-Location
-
-cd DungineX
-$totalC = (dir -include *.cpp,*.c -recurse | select-string .).Count
-$totalH = (dir -include *.h -recurse | select-string .).Count
-
-cd Vendor
-$vendorC = (dir -include *.cpp,*.c -recurse | select-string .).Count
-$vendorH = (dir -include *.h -recurse | select-string .).Count
-
-$actualC = $totalC - $vendorC
-$actualH = $totalH - $vendorH
-
-Write-Host "Total C/C++ files: $actualC"
-Write-Host "Total     H files: $actualH"
-
-cd $pwd
+cloc --vcs=git --exclude-list-file=./DungineX/Vendor/
